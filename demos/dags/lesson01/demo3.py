274d425f027b38811a16e524a07b885eb58e1586
jaiswalanshul <anshul.chander@gmail.com>
2020-02-24 11:17:17 +1100

    added lesson01 demo3

---
+++ demos/dags/lesson01/demo3.py
@@ -0,0 +1,37 @@
+import datetime
+import logging
+import os
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+def hell_world():
+    logging.info("hello world")
+
+def current_time():
+    logging.info(f"Working directory is {os.getcwd()}")
+
+def complete():
+    logging.info("Congrats your first multi-task pipline is now complete!")
+
+dag = DAG(
+    "lesson1.demo3",
+    schedule_interval = "@hourly"
+    start_date=datetime.datetime.now() - datetime.timedelta(days=1))
+
+hello_worl_task = PythonOperator(
+    task_id = "hello_world",
+    python_callable=hello_world,
+    dag=dag)
+
+# TODO: Create Tasks and Operators
+
+
+#
+# TODO: Configure the task dependencies such that the graph looks like the following:
+#
+#                    -> current_time_task
+#                   /                    \
+#   hello_world_task                      -> division_task
+#                   \                    /
+#                    -> working_dir_task
