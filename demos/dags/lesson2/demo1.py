0ebf08b3f83a9171b7b77bef5ae21d7d3eb4d24d
jaiswalanshul <anshul.chander@gmail.com>
2020-02-24 11:55:04 +1100

    added lesson02 demo1

---
+++ demos/dags/lesson2/demo1.py
@@ -0,0 +1,52 @@
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
+
+import sql
+
+
+def load_trip_data_to_redshift(*args, **kwargs):
+    aws_hook = AwsHook("aws_credentials")
+    credentials = aws_hook.get_credentials()
+    redshift_hook = PostgresHook("redshift")
+    sql_stmt = sql.COPY_ALL_TRIPS_SQL.format(
+        credentials.access_key,
+        credentials.secret_key,
+    )
+    redshift_hook.run(sql_stmt)
+
+
+dag = DAG(
+    'lesson2.demo1',
+    start_date=datetime.datetime.now()
+)
+
+create_trips_table = PostgresOperator(
+    task_id="create_trips_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql.CREATE_TRIPS_TABLE_SQL
+)
+
+copy_trips_task = PythonOperator(
+    task_id='load_trips_from_s3_to_redshift',
+    dag=dag,
+    python_callable=load_trip_data_to_redshift,
+)
+
+location_traffic_task = PostgresOperator(
+    task_id="calculate_location_traffic",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql.LOCATION_TRAFFIC_SQL
+)
+
+create_trips_table >> copy_trips_task
+# TODO: First, load the Airflow UI and run this DAG once.
+# TODO: Next, configure the task ordering to add the location traffic task
+#       Then, run this DAG once more and inspect the run history.
